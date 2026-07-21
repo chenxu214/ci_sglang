fbce7c58c5c28624e183b156f493ce469d19f777
khalil2ji3mp6 <khalilzhk@gmail.com>
2026-07-21 14:58:34 +0800

    0721 init

---
+++ run_16p_mix.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=10
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+export SGLANG_SET_CPU_AFFINITY=1
+export SGLANG_ONE_VISIBLE_DEVICE_PER_PROCESS=1
+
+MODEL_PATH=/home/weights/Kimi-K3-int4
+
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+unset ASCEND_LAUNCH_BLOCKING
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+export HCCL_SOCKET_IFNAME=lo
+export GLOO_SOCKET_IFNAME=lo
+export STREAMS_PER_DEVICE=32
+
+export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
+
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
+export HCCL_BUFFSIZE=1200
+export HCCL_OP_EXPANSION_MODE=AIV
+
+export PYTHONPATH=/home/zkk/sglang/python:$PYTHONPATH
+
+D_IP=('192.168.25.209' '192.168.25.217')
+LOCAL_HOST1=`hostname -I|awk -F " " '{print$1}'`
+LOCAL_HOST2=`hostname -I|awk -F " " '{print$2}'`
+echo "${LOCAL_HOST1}"
+echo "${LOCAL_HOST2}"
+
+for i in "${!D_IP[@]}";
+do
+    if [[ "$LOCAL_HOST1" == "${D_IP[$i]}" || "$LOCAL_HOST2" == "${D_IP[$i]}" ]];
+    then
+        echo "Decode -> ${D_IP[$i]}"
+
+        export HCCL_SOCKET_IFNAME=enp196s0f0
+        export GLOO_SOCKET_IFNAME=enp196s0f0
+
+        sglang serve \
+            --model-loader-extra-config '{"enable_multithread_load": true}' \
+            --dist-init-addr 192.168.25.209:5000 --nnodes 2 --node-rank $i \
+            --model-path $MODEL_PATH \
+            --tokenizer-path /home/weights/Kimi-K3 \
+            --trust-remote-code \
+            --attention-backend ascend \
+            --device npu \
+            --quantization modelslim \
+            --dtype bfloat16 \
+            --tp-size 32 \
+            --mem-fraction-static 0.8 \
+            --chunked-prefill-size -1 \
+            --disable-cuda-graph \
+            --disable-radix-cache \
+            --host 0.0.0.0 \
+            --port 30000 \
+            --skip-server-warmup
+
+        exit 1
+    fi
+done
+
+exit 1
+
