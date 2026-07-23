e2c68f507ce65e633d455c33320a196eceadf84b
chenxu214 <justin_cc2025@163.com>
2026-07-23 23:31:52 +0800

    bugfix

--- python/sglang/srt/models/kimi_k3.py
+++ python/sglang/srt/models/kimi_k3.py
@@ -21,11 +21,8 @@ from sglang.srt.distributed import (
 )
 from sglang.srt.eplb.expert_distribution import get_global_expert_distribution_recorder
 from sglang.srt.layers.attention.fla.fused_norm_gate import FusedRMSNormGated
-<<<<<<< HEAD
 from sglang.srt.layers.attention.fla.kda import fused_kda_gate
-=======
 from sglang.srt.layers.attention.vision import VisionAttention
->>>>>>> 7fe1a1aa9 (support image_url input)
 from sglang.srt.layers.communicator import AttentionInputs, get_attn_tp_context
 from sglang.srt.layers.conv import Conv2dLayer
 from sglang.srt.layers.dp_attention import is_dp_attention_enabled
